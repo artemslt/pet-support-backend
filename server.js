70fb3314169948058c0a4b5a502201efb4ef00c1
artemslt <ppt.artem@gmail.com>
2023-02-15 15:52:57 +0200

    start

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT;
+
+app.listen(PORT || 3000, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
