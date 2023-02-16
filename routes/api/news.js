ebea14414c1f6d185c7baaf1403b19cb8efd4b07
Valeriia Masiuk <valmas1997@gmail.com>
2023-02-17 00:28:16 +0200

    adds routes to get friends list and news list

---
+++ routes/api/news.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router();
+
+const { getAllNews } = require("../../controllers/controllers");
+const {ctrlWrapper} = require("../../middleware/ctrlWrapper")
+
+router.get('/', ctrlWrapper(getAllNews))
+
+module.exports = router
