62aab8a72c84521b88ece7159e350ecb27332607
Valeriia Masiuk <valmas1997@gmail.com>
2023-02-17 15:56:08 +0200

    додає перевірку на наявність даних в масиві

--- controllers/news/getAllNews.js
+++ controllers/news/getAllNews.js
@@ -1,8 +1,12 @@
 const New = require("../../models/news");
+const { NotFound } = require("http-errors");
 
 const getAllNews = async (req, res, next) => {
-    const news = await New.find();
-    res.json(news);
-}
+  const news = await New.find();
+  if (news.length === 0) {
+    throw NotFound("There is no news added")
+  }
+  res.json(news);
+};
 
 module.exports = getAllNews;
