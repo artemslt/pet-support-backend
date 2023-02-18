04e8b73d8ead507c91686f2d106fc81da19c56a4
artemslt <ppt.artem@gmail.com>
2023-02-18 23:21:03 +0200

    add auth swagger

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -16,6 +18,8 @@ const usersRouter = require('./routes/api/users');
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
