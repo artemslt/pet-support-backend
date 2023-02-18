36e80a796250f4a583bc926e21f62d45ec0a9970
Artem Karpenko <dante120591@gmail.com>
2023-02-18 12:25:38 +0200

    Додав базову авторизацію
    + реєстрація
    + логін
    + логаут
    + current user

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-const newsRouter = require("./routes/api/news");
-const friendsRouter = require("./routes/api/friends");
+const newsRouter = require('./routes/api/news');
+const friendsRouter = require('./routes/api/friends');
+const authRouter = require('./routes/api/auth');
+const usersRouter = require('./routes/api/users');
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/news", newsRouter);
-app.use("/api/friends", friendsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/news', newsRouter);
+app.use('/api/friends', friendsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
@@ -27,5 +31,3 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-
-
---
+++ routes/api/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const { auth } = require('../../middleware/auth');
+// const { upload } = require('../../middleware/upload');
+const { ctrlWrapper } = require('../../middleware/ctrlWrapper');
+const { users: ctrl } = require('../../controllers');
+const router = express.Router();
+
+router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
+// router.patch(
+//   '/avatars',
+//   auth,
+//   upload.single('avatar'),
+//   ctrlWrapper(ctrl.updateAvatar)
+// );
+// router.get('/verify/:verificationToken', auth, ctrlWrapper(ctrl.verifyEmail));
+
+module.exports = router;
