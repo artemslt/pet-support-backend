cafca45b3bd76ca91a2fdde465e1302433538f1f
Artem Karpenko <dante120591@gmail.com>
2023-02-18 20:28:29 +0200

    Додає отримання всієї інфи по користувачу і петам

--- controllers/users/getFullInfo.js
+++ controllers/users/getFullInfo.js
@@ -1,11 +1,16 @@
+const { Pet } = require('../../models/pet');
+
 const getFullInfo = async (req, res) => {
   const user = req.user;
+  const { _id } = req.user;
+  const pets = await Pet.find({ owner: _id }).populate(
+    'owner',
+    '_id name email'
+  );
   res.json({
     status: 'success',
     code: 200,
-    data: {
-      user: user,
-    },
+    data: { user, pets },
   });
 };
 
