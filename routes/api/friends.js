21eda038726a49ddde31a7087ab9603fd0d5b23a
Artem Karpenko <dante120591@gmail.com>
2023-02-18 14:09:13 +0200

    bugfix
    Ненароком виправив те, що не мав чіпати. Повернув як було.

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getAllFriends } = require('../../controllers/friends/getAllFriends');
+const { getAllFriends } = require('../../controllers/controllers');
 const { ctrlWrapper } = require('../../middleware/ctrlWrapper');
 
 router.get('/', ctrlWrapper(getAllFriends));
