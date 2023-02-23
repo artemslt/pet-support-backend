e3483febca5c78e62f4380790b8ab44985723a98
Artem Karpenko <dante120591@gmail.com>
2023-02-23 11:19:33 +0200

    Вносить правки в гугл авторизацію
    А також дрібний фікс дефолтної локації, і (сподіваюсь) прибирає помилку в консолі на GET /users/current

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,12 +1,16 @@
 const getCurrent = async (req, res) => {
-  const user = req.user;
-  res.json({
-    status: 'success',
-    code: 200,
-    data: {
-      user,
-    },
-  });
+  try {
+    const user = req.user;
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
 };
 
 module.exports = getCurrent;
