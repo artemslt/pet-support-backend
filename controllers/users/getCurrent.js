3d475aaf7c5bf12250463b84d51e53a4da159b68
Artem Karpenko <dante120591@gmail.com>
2023-02-27 15:15:15 +0200

    Фікс фіксу

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,11 +1,12 @@
 const getCurrent = async (req, res) => {
   try {
-    const { name, email, birthday, phone, location, avatarURL } = req.user;
+    const { name, email, birthday, phone, location, avatarURL, favorite } =
+      req.user;
     res.json({
       status: 'success',
       code: 200,
       data: {
-        user: { name, email, birthday, phone, location, avatarURL },
+        user: { name, email, birthday, phone, location, avatarURL, favorite },
       },
     });
   } catch (error) {
