6a1a58c9693bea632fa22ca5e004b47b1907c417
Artem Karpenko <dante120591@gmail.com>
2023-02-27 22:05:44 +0200

    Додає редірект на доки

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const newsRouter = require('./routes/api/news');
 const friendsRouter = require('./routes/api/friends');
 const authRouter = require('./routes/api/auth');
 const usersRouter = require('./routes/api/users');
-const noticesRouter = require("./routes/api/notices");
+const noticesRouter = require('./routes/api/notices');
+const redirectToDocs = require('./middleware/redirectToDocs');
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use('/', redirectToDocs);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/auth', authRouter);
---
+++ middleware/redirectToDocs.js
@@ -0,0 +1,5 @@
+const redirectToDocs = async (req, res) => {
+  res.redirect('https://pet-support-backend-v8vc.onrender.com/api-docs');
+};
+
+module.exports = redirectToDocs;
