6e8119202f7a1007c7a695fc7e73d59e951d63af
Artem Karpenko <dante120591@gmail.com>
2023-03-01 10:53:44 +0200

    Видаляє костиль для записування дефолтних значень
    (костиль буде на фронті)

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,14 +1,5 @@
 const validation = schema => {
   return (req, res, next) => {
-    if (req.body.name === '') {
-      req.body.name = 'New User';
-    }
-    if (req.body.location === '') {
-      req.body.location = 'City, Region';
-    }
-    if (req.body.phone === '') {
-      req.body.phone = '+380000000000';
-    }
     const { error } = schema.validate(req.body);
     if (error) {
       res.status(400).json({ message: `${error}` });
