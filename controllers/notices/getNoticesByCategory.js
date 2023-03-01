33112418dcc1922a2339b93a25e2ec9b3a623f92
Valeriia Masiuk <valmas1997@gmail.com>
2023-03-01 09:00:09 +0200

    fix

--- controllers/notices/getNoticesByCategory.js
+++ controllers/notices/getNoticesByCategory.js
@@ -2,7 +2,7 @@ const Notice = require('../../models/notice');
 
 const getNoticesByCategory = async (req, res) => {
   const { category } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 40 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Notice.find({ category }, '', {
     skip,
@@ -13,13 +13,18 @@ const getNoticesByCategory = async (req, res) => {
     return res
       .status(404)
       .json({ message: 'There is no notices in this category' });
-  }
+  };
+
+  const notices = [...result].sort(
+    (firstNotice, secondNotice) =>
+      new Date(secondNotice.createdAt) - new Date(firstNotice.createdAt)
+  );
 
   res.json({
     status: 'success',
     code: 200,
     data: {
-      result,
+      notices,
     },
   });
 };
